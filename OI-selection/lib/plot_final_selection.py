4aa0d4096b615fbc9539d39e4d02b151724f201f
Gerard O'Reilly <gerard.oreilly@iusspavia.it>
2020-02-20 19:19:24 +0100

    Added more information to output plots

--- OI-selection/lib/plot_final_selection.py
+++ OI-selection/lib/plot_final_selection.py
@@ -1,22 +1,75 @@
-def plot_final_selection(name,nGM,TgtPer,sampleSmall,meanReq,stdevs,meanrecorded,output_folder):
+def plot_final_selection(name,lbl,nGM,TgtPer,T_CS,sampleSmall,meanReq,stdevs,meanrecorded,meanrecorded_p2sigma,meanrecorded_n2sigma,meanrecorded_eps,output_folder):
 	# Import libraries
 	import numpy as np
 	import matplotlib.pyplot as plt
 	
+#	plt.figure()
+#	for i in np.arange(nGM):
+#		plt.loglog(TgtPer,np.exp(sampleSmall[i,:]),'g')
+#	plt.loglog(TgtPer,np.exp(meanReq),'r',label='target')
+#	plt.loglog(TgtPer,np.exp(meanReq+2*stdevs),'--r',label='target+2*sigma')
+#	plt.loglog(TgtPer,np.exp(meanReq-2*stdevs),'--r',label='target-2*sigma')
+#	plt.loglog(TgtPer,meanrecorded,'b',label='mean recorded')
+#	plt.xlabel('Period (s)')
+#	plt.ylabel('Spectral acceleration (g)')
+#	plt.xlim((0.01, np.max(TgtPer)))
+#	plt.legend()
+#	plt.grid(True)
+#	plt.savefig(output_folder+'/'+name+'/'+name+'_selection.png', bbox_inches='tight')
+#	plt.close()
+
+	# Spectra with ground motions
 	plt.figure()
+	plt.rcParams.update({'font.size': 8})
 	for i in np.arange(nGM):
 		plt.loglog(TgtPer,np.exp(sampleSmall[i,:]),'g')
-	plt.loglog(TgtPer,np.exp(meanReq),'r',label='target')
-	plt.loglog(TgtPer,np.exp(meanReq+2*stdevs),'--r',label='target+2*sigma')
-	plt.loglog(TgtPer,np.exp(meanReq-2*stdevs),'--r',label='target-2*sigma')
-	plt.loglog(TgtPer,meanrecorded,'b',label='mean recorded')
-	plt.xlabel('Period (s)')
-	plt.ylabel('Spectral acceleration (g)')
-	plt.xlim((0.01, np.max(TgtPer)))
-	plt.legend()
-
+	plt.plot(T_CS,np.exp(meanReq),'r',label='CMS')
+	plt.plot(T_CS,np.exp(meanReq+2*stdevs),'--r',label=r'CMS $\pm 2\sigma$')
+	plt.plot(T_CS,np.exp(meanReq-2*stdevs),'--r')
+	plt.plot(TgtPer,meanrecorded,'k',label='Selected')
+	plt.plot(TgtPer,meanrecorded_p2sigma,'--k',label=r'Selected $\pm 2\sigma$')
+	plt.plot(TgtPer,meanrecorded_n2sigma,'--k')
+	plt.xlabel('Period [s]')
+	plt.ylabel('Intensity Measure - '+lbl)
+	plt.xlim(min(T_CS),max(T_CS))
+	plt.ylim(1e-2,1e1)
+	plt.yscale('log')
+	plt.xscale('log')
 	plt.grid(True)
-
-	plt.savefig(output_folder+'/'+name+'/'+name+'_selection.png', bbox_inches='tight')
-
+	plt.savefig(output_folder+'/'+name+'/'+name+'_spectra_gms.pdf', bbox_inches='tight')
+	plt.close()
+	
+	# Spectra 
+	plt.figure()
+	plt.rcParams.update({'font.size': 8})
+	plt.plot(T_CS,np.exp(meanReq),'r',label='CMS')
+	plt.plot(T_CS,np.exp(meanReq+2*stdevs),'--r',label=r'CMS $\pm 2\sigma$')
+	plt.plot(T_CS,np.exp(meanReq-2*stdevs),'--r')
+	plt.plot(TgtPer,meanrecorded,'k',label='Selected')
+	plt.plot(TgtPer,meanrecorded_p2sigma,'--k',label=r'Selected $\pm 2\sigma$')
+	plt.plot(TgtPer,meanrecorded_n2sigma,'--k')
+	plt.xlabel('Period [s]')
+	plt.ylabel('Intensity Measure - '+lbl)
+	plt.xlim(min(T_CS),max(T_CS))
+	plt.ylim(1e-2,1e1)
+	plt.yscale('log')
+	plt.xscale('log')
+	plt.grid(True)
+	plt.legend()
+	plt.savefig(output_folder+'/'+name+'/'+name+'_spectra.pdf', bbox_inches='tight')
+	plt.close()
+	
+	# Epsilon
+	plt.figure()
+	plt.rcParams.update({'font.size': 8})
+	plt.plot(T_CS,stdevs,'r',label='CMS')
+	plt.plot(TgtPer,meanrecorded_eps,'k',label='Selected')
+	plt.xlabel('Period [s]')
+	plt.ylabel('Dispersion')
+	plt.xlim(min(T_CS),max(T_CS))
+	plt.ylim(0,1)
+	plt.xscale('log')
+	plt.grid(True)
+	plt.legend()
+	plt.savefig(output_folder+'/'+name+'/'+name+'_dispersion.pdf', bbox_inches='tight')
 	plt.close()
